10eb8895e84712d23149bd8b162baaf858be6f50
ahmed14-cell <ahmedito14@gmail.com>
2020-10-26 12:46:36 +0300

    added more functionality

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+from __future__ import print_function
 import os
+import flask
 from flask import Flask, request, render_template
 import africastalking
 
@@ -8,6 +10,7 @@ api_key = os.environ.get('KEY')
 
 africastalking.initialize(username, api_key)
 airtime = africastalking.Airtime
+
 
 
 @app.route('/', methods=['POST','GET'])
@@ -28,5 +31,34 @@ def index():
     
     return render_template('index.html')
 
+class AIRTIME:
+    def __init__(self):
+		# Set your app credentials
+        self.username = username
+        self.api_key = api_key
+
+        # Initialize the SDK
+        africastalking.initialize(self.username, self.api_key)
+
+        # Get the airtime service
+        self.airtime = africastalking.Airtime
+
+    def send(self):
+        # Set phone_number in international format
+        phone_number = 'MyPhoneNumber'
+
+        # Set The 3-Letter ISO currency code and the amount
+        amount = "908"
+        currency_code = "KES"
+
+        try:
+			# That's it hit send and we'll take care of the rest
+            responses = self.airtime.send(phone_number=phone_number, amount=amount, currency_code=currency_code)
+            print (responses)
+        except Exception as e:
+            print ("Encountered an error while sending airtime:%s" %str(e))
+
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port = 3030)
+    AIRTIME().send()
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port = 3030, debug=True)
